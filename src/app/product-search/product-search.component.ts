df5f27b1e2681fd8de9f68a72483b68f9d58555b
danielmachado86 <danielmcis@gmail.com>
2020-06-20 09:21:21 -0500

    Basic search capabilities added

--- src/app/product-search/product-search.component.ts
+++ src/app/product-search/product-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service'
+import { Observable, Subject } from 'rxjs';
+import { Product } from '../product';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-search',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSearchComponent implements OnInit {
 
-  constructor() { }
+  products$:Observable<Product[]>;
+  private searchTerms = new Subject<string>();
+
+
+  constructor(private productService: ProductService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.products$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.productService.searchProducts(term)),
+    );
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -23,23 +23,37 @@ export class ProductService {
     );
   }
 
-  /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /* GET heroes whose name contains search term */
+  searchProducts(term: string): Observable<Product[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Product[]>(`${this.API_URL}/search?q=${term}`).pipe(
+      tap(x => x.length ?
+        console.log(`found heroes matching "${term}"`) :
+        console.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Product[]>('searchHeroes', []))
+    );
+  }
 }
