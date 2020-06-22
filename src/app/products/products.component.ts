b7487c8007f8f7ae92904e103abb14dd9d7f3c05
danielmachado86 <danielmcis@gmail.com>
2020-06-21 22:38:01 -0500

    In-line Product detail display added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
@@ -9,9 +9,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[];
+  @ViewChildren("boxes") private boxes: QueryList<ElementRef>;
 
-  constructor(private productService:ProductService) { }
+  products: Product[];
+  selectedProduct: Product;
+  prevIndex: number;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
@@ -19,7 +23,27 @@ export class ProductsComponent implements OnInit {
 
   getProducts(): void {
     this.productService.getProducts()
-        .subscribe(products => this.products = products);
+      .subscribe(products => this.products = products);
   }
 
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+  }
+
+  toggleBox(index: number) {
+    this.selectedProduct = this.products[index];
+    this.productDetailDisplay(index);
+    if (this.prevIndex != null) {
+      this.productDetailDisplay(this.prevIndex);
+    }
+    this.prevIndex = index;
+  }
+
+  private productDetailDisplay(index: number) {
+    let nativeElement = this.boxes.toArray()[index].nativeElement;
+    nativeElement.style.display =
+      nativeElement.style.display === "none" || !nativeElement.style.display
+        ? "block"
+        : "none";
+  }
 }
