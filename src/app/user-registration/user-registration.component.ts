633f2f71d6207b1d2649a0917c1bdc60aba71762
danielmachado86 <danielmcis@gmail.com>
2020-07-20 08:43:51 -0500

    Added backend integration

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-import { User } from "../user";
 import { UserService } from '../user.service';
-import { HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.css']
+  styleUrls: ['./user-registration.component.scss']
 })
 export class UserRegistrationComponent implements OnInit {
+  responseStatus: number;
+  isAuthenticated: boolean;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private authService: KeycloakService,
+    private router: Router) { }
 
-  user: User
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
   
   ngOnInit(): void {
   }
 
-  onSubmit() {this.userService.addUser(this.user)}
-
-  showFormControls(form: any) {
-    return form && form.controls['name'] &&
-    form.controls['name'].value;
+  onSubmit() { 
+    this.userService.addUser(this.userForm.value).subscribe(
+      response => {
+        this.responseStatus = response.status;
+      },
+      (error: Response) => this.responseStatus = error.status
+   )
   }
 
 }
