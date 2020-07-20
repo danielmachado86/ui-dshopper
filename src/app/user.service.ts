914c707d3d5a3cd798fe5ea54a5ea1416a1f2426
danielmachado86 <danielmcis@gmail.com>
2020-07-20 08:53:36 -0500

    Added get account info function

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,32 +1,54 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from './user';
-import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/xml',
-    'Authorization': 'jwt-token'
-  })
-};
+import { Router } from '@angular/router';
+import { catchError, map, tap } from 'rxjs/operators';
+import { User } from './user';
+import { KeycloakService } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  $authenticationState: any;
+  extractData: any;
+  user: any;
+  user$: Observable<void>;
+
   private API_URL= environment.USERS_API_URL;
   private usersUrl = this.API_URL + '/users'; 
 
+
   constructor(private http: HttpClient) { }
 
-  addUser(user: User): Observable<User> {
-    return this.http.post<User>(this.usersUrl, user, httpOptions)
+  addUser(body): Observable<HttpResponse<Response>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      }),
+      observe: 'response' as const
+    };
+    return this.http.post<Response>(this.usersUrl, body, httpOptions)
       .pipe(
-        catchError(this.handleError('addUser', user))
+        catchError(this.handleError('addUser', body))
       );
+  }
+
+
+
+  getUser(httpHeaderWithToken: HttpHeaders): Observable<any> {
+    console.log("Intentando solicitar user info con headers " + httpHeaderWithToken);
+    const getHttpOptions = {
+      headers: httpHeaderWithToken
+    };
+    return this.http.get(`${this.usersUrl}/me`, getHttpOptions).pipe(
+      tap(x => x ?
+        console.log(`found user matching`) :
+        console.log(`no user matching`)),
+      catchError(this.handleError<User>('getUser', this.user))
+    );
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
