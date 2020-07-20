a9de141aba853245be44bd1c1f06724d22563097
danielmachado86 <danielmcis@gmail.com>
2020-07-20 08:46:01 -0500

    Minor styling applied

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,7 +4,7 @@ import { Product } from '../product';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
 
