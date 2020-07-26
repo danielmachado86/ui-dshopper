3f6b034700a2e3d782133d47a3e031f67fc63fc4
danielmachado86 <danielmcis@gmail.com>
2020-07-25 21:16:52 -0500

    Minor change

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-user-account',
   templateUrl: './user-account.component.html',
-  styleUrls: ['./user-account.component.css']
+  styleUrls: ['./user-account.component.scss']
 })
 export class UserAccountComponent implements OnInit {
 
